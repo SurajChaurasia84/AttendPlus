9bb0467936e53919f30f46646ceb4af5800eba10
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-01-18 16:30:15 +0530

    fix bug

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,9 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -42,4 +43,7 @@ android {
 
 flutter {
     source = "../.."
+}
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
